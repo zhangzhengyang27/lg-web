8e71b9b3feb88b72bd629aed4a2a23dd463d5f2b
zhangzhengyang <1074385735@qq.com>
2022-04-20 10:55:52 +0800

    前端工程化

--- 01-02-study-materials/01-02-codes/01-02-03-01-js-performance/01-keyword-variable.js
+++ 01-02-study-materials/01-02-codes/01-02-03-01-js-performance/01-keyword-variable.js
@@ -1,17 +1,27 @@
+function fn() {
+    name = 'lg'
+    console.log(`${name} is a coder`)
+}
+
+fn()
 
 
 function fn() {
-  name = 'lg'
-  console.log(`${name} is a coder`)
+    const name = 'lg'
+    console.log(`${name} is a coder`)
 }
 
-fn() 
+fn()
 
+const user1={age:11}
+const user2={age:11}
+const user3={age:11}
+
+const nameList=[user1.age,user2.age,user3.age]
 
 function fn() {
-  const name = 'lg'
-  console.log(`${name} is a coder`)
+    const num1=1;
+    const num2=1;
 }
-
-fn() 
+fn()
 
---
+++ 01-02-study-materials/01-02-codes/01-02-03-01-js-performance/1.js
@@ -0,0 +1,19 @@
+let aee = [{
+    "type": "Keyword",
+    "value": "const"
+},
+    {
+        "type": "Identifier",
+        "value": "username"
+    }
+    ,
+    {
+        "type": "Punctuator",
+        "value": "="
+    }
+    ,
+    {
+        "type": "String",
+        "value": "alishi"
+    }
+]
